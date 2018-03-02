3ac3d89a25db361f0ea0cf66fe39774530485b0b
Fanny Hasbi <fnyhsbi@gmail.com>
2018-03-03 02:15:15 +0700

    Redesign detail obat page

--- application/views/home/detail_obat.php
+++ application/views/home/detail_obat.php
@@ -2,15 +2,25 @@
   <h2><?= $obat->nama ?><small>(<?= $obat->kode_obat;?>)</small></h2>
 </div>
 
-<div class="container">
+<div class="container-fluid">
+  <table class="table table-striped">
+    <tbody>
+      <tr>
+        <td>Bentuk obat</td>
+        <td>Dikonsumsi oleh</td>
+        <td>Harga<small>/pcs</small></td>
+      </tr>
+      <tr>
+        <td><?= $obat->bentuk;?></td>
+        <td><?= $obat->konsumen;?></td>
+        <td><?= number_format($obat->harga, 0, ',', '.'); ?></td>
+      </tr>
+    </tbody>
+  </table>
+
   <h3>Manfaat</h3>
   <p><?= $obat->manfaat;?></p>
-  <ul>
-    <li>Bentuk obat : <?= $obat->bentuk;?></li>
-    <li>Dikonsumsi oleh : <?= $obat->konsumen;?></li>
-    <li>Harga satuan : <?= $obat->harga;?></li>
-  </ul>
 
-  <br>
+  <hr>
   <a href="javascript:history.go(-1)" class="btn btn-warning">Kembali</a>
 </div>

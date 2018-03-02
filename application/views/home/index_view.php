9767324772d87d44750064c1ec9e0fc3a975c0e8
Fanny Hasbi <fnyhsbi@gmail.com>
2018-03-03 01:27:54 +0700

    Move asset call to top

--- application/views/home/index_view.php
+++ application/views/home/index_view.php
@@ -12,6 +12,11 @@
   <link rel="stylesheet" type="text/css" href="<?= base_url();?>assets/dist/css/skins/skin-blue-light.min.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
+  <!-- jQuery 3 -->
+  <script src="<?= base_url();?>assets/bower_components/jquery/dist/jquery.min.js"></script>
+  <!-- Bootstrap 3.3.7 -->
+  <script type="text/javascript" src="<?= base_url();?>assets/js/bootstrap.min.js"></script>
+
   <!-- DataTable -->
   <link rel="stylesheet" href="<?= base_url(); ?>/assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css"/>
 </head>
@@ -75,10 +80,6 @@
 </div>
 <!-- ./wrapper -->
 
-<!-- jQuery 3 -->
-<script src="<?= base_url();?>assets/bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script type="text/javascript" src="<?= base_url();?>assets/js/bootstrap.min.js"></script>
 <!-- DataTable -->
 <script src="<?= base_url(); ?>/assets/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="<?= base_url(); ?>/assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
